da1f83f7b4adb5c8c52017795241763f4d34195d
agustinpardo2001 <93058814+agustinpardo2001@users.noreply.github.com>
2021-10-23 20:10:50 -0300

    Add files via upload

---
+++ clase1.js
@@ -0,0 +1,9 @@
+//TAREA NRO 1//
+let numeroA = prompt("Ingrese el primer numero que desea sumar");
+let numeroB = prompt("Ingrese el segundo numero que desea sumar");
+
+let suma = parseInt(numeroA) + parseInt(numeroB);
+
+alert("El resultado de la suma es: " + suma);
+//TAREA NRO 1//
+
